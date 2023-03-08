8629121e0767cfeecc6bf6e3fff60844f86042fc
Dimitris Kavroudakis <dimitris123@gmail.com>
2023-03-08 08:27:42 +0200

    Update PointPattern_density.r

--- R/Spatial_Analysis/PointPattern_density.r
+++ R/Spatial_Analysis/PointPattern_density.r
@@ -1,31 +1,31 @@
 
-# Make a point pattern for further analysis.
+# Generate a point pattern for further analysis.
 makePointPattern = function(Xs,Ys){
   library(spatstat)
   win=owin(range(Xs),range(Ys))
   return(ppp(Xs,Ys,window=win))
 }
 
-#Data
+# Data
 x=c(10,4,2,5,7,8,9)
 y=c(4,5,6,7,8,9,6)
 
-#Make a point pattern
+# Make a point pattern
 pp=makePointPattern(x,y)
 
-#make a density of the points with 0.6 smoothing bandwith.
+# make a density of the points with 0.6 smoothing bandwith.
 den=density(pp,0.6)
 
-#Plot all
+# Plot all
 plot(den)
 contour(den,  add=T)
 plot(pp, add=T, pch=17)
 
-#For Gaussian kernel the bw tag can be used.
+# For Gaussian kernel the bw tag can be used.
 # bw.nrd is the more common variation given by Scott (1992), using factor 1.06.
 den=density(pp, bw='nrd')
 
-#Plot all
-#plot(den)
-#contour(den,  add=T)
-#plot(pp, add=T, pch=17)
+# Plot all
+plot(den)
+contour(den,  add=T)
+plot(pp, add=T, pch=17)
